13cf65e54b2631aa13eb054691f587cf6fa13bf1
Robert S <112721454+roberts-fullstack@users.noreply.github.com>
2022-11-08 13:33:25 -0500

    new files

--- main.js
+++ main.js
@@ -71,3 +71,150 @@ while (count <= gridWidth * gridWidth) {
 // You'll need to add the appropriate event listener for each
 // square and for each palette color from the functions you
 // wrote above.
+/*******************
+ * OUR HELPER CODE *
+*******************/
+
+/*
+ * Here we add the squares to the canvas dynamically.
+ * You can mostly leave this section alone!
+ * But if you want to change how wide the canvas is,
+ * there are just two steps:
+ * 
+ * 1. Change the `gridWidth` value below.
+ * 2. Change the `grid-template-rows` and
+ * `grid-template-columns` to match.
+ *
+ * To make the second one happen, the number to change
+ * is the first argument to `repeat`, currently set at 10.
+ */
+const gridWidth = 20;
+let count = 0;
+while (count <= gridWidth * gridWidth) {
+  const canvas = document.querySelector('.canvas');
+  const div = document.createElement('div');
+  div.className = 'square color-5';
+  canvas.appendChild(div);
+  count++;
+}
+
+// You probably should NOT do these in the order below.
+// That is, you probably should NOT do all the queries,
+// THEN all the functions,
+// THEN all the wiring.
+
+// Instead, it'll be easier if you go one action at a time!
+// So, add a query for the palette colors.
+// THEN add an event listener function for what happens when one is clicked.
+// THEN wire those two together, so that when the palette elements are clicked,
+// the function runs.
+//
+// And proceed from there to getting the squares working.
+//
+
+// ALSO.
+// You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
+
+/***********
+ * QUERIES *
+***********/
+
+// Add queries for all your squares, palette colors, and brush here.
+// (Note the singular or plural used in that sentence!)
+
+let brush = document.querySelector('.current-brush');
+
+let app = document.querySelector('.app');
+
+let palette = document.querySelectorAll('.palette div');
+
+let canvasSquares = document.querySelectorAll('.canvas div');
+
+
+
+
+/****************************
+ * EVENT LISTENER FUNCTIONS *
+****************************/
+
+// Now add some functions to handle clicking one particular square
+// and clicking one particular palette color. You can leave them
+// empty at first, though a console.log just to know they're being
+// run as event listeners (after the next step is set up) isn't a
+// bad idea for testing purposes.
+
+brush.addEventListener('click', function(){
+
+  console.log("current brush");
+});
+
+for(let i = 0; i < palette.length; i++){
+  palette[i].addEventListener('click', function(){
+    console.log(palette[i].classList);
+    brush.classList.replace(brush.classList[1],palette[i].classList[1]);
+  });
+}
+
+for(let i = 0; i < canvasSquares.length; i++){
+ canvasSquares[i].addEventListener('click', function(){
+  console.log(canvasSquares[i].classList);
+  canvasSquares[i].classList.replace(canvasSquares[i].classList[1], brush.classList[1]);
+ });
+
+}
+
+let isMouseDown = false;
+for(let square of canvasSquares){
+{
+  square.addEventListener('click', function(){
+    // console.log(square.classList);
+    isMouseDown = false;
+    square.classList.replace(square.classList[1], brush.classList[1]);
+  })
+}
+}
+
+
+// mouseenter code will be below
+for(let square of canvasSquares){
+// square.addEventListener('mouseenter', function(){
+//   square.classList.replace(square.classList[1], brush.classList[1]);
+// });
+
+
+square.addEventListener('mouseover', function(){
+  if(isMouseDown === true) {
+    square.classList.replace(square.classList[1],brush.classList[1]);
+  }
+})
+}
+//mousedown - checks if user left clicks and holds
+//mouseup - checks for when user releases that left click
+
+app.addEventListener('mousedown', function(){
+  console.log('Mouse is down');
+  isMouseDown = true;
+  console.log(`isMouseDown: ${isMouseDown}`);
+});
+
+app.addEventListener('mouseup', function(){
+  console.log('Mouse is up');
+  isMouseDown = false;
+  console.log(`isMouseDown: ${isMouseDown}`);
+});
+
+
+
+
+
+
+
+
+/**************************
+ * WIRING IT ALL TOGETHER *
+**************************/
+
+// Now: wiring up our event listeners to our html node elements.
+// You'll need to add the appropriate event listener for each
+// square and for each palette color from the functions you
+// wrote above.
